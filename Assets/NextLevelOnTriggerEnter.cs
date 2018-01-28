7700d127dda6d91aca6c753c54f66c8b5c1339e2
Sam Tregillus <samtregillus@gmail.com>
2018-01-28 11:39:23 -0800

    this is a commit message yeah

---
+++ Assets/NextLevelOnTriggerEnter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelOnTriggerEnter : MonoBehaviour {
+
+    public int buildNumber = 0;
+
+    public bool justDoNextBuildNumber = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            int nextSceneIndex = buildNumber;
+            if (justDoNextBuildNumber)
+            {
+                nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            }
+            SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(nextSceneIndex));
+
+        }
+    }
+}
